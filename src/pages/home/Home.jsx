df439f1aac3b6b36f9e00c2842dc6a9b2d9cc3a0
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-03-15 10:07:06 -0500

    React-admin projects started, folder and routes setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      <h1 className="title">React Admin</h1>
+    </div>
+  );
+};
+
+export default Home;
