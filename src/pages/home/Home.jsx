6330766b35b66b798cca9efcebb8d15f67529de9
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-03-15 12:28:23 -0500

    Navbar and sidebar done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
 import "./home.scss";
 
 const Home = () => {
   return (
-    <div className="homeContainer">
-      <h1 className="title">React Admin</h1>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          
+        </div>
+      </div>
     </div>
   );
 };
