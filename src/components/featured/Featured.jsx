1842249979dea9a64bea68953887c05f04584c31
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-04-15 20:25:56 -0500

    woking on Home page widget

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,31 @@
-import './featured.scss'
+import "./featured.scss";
+import { CircularProgressbar } from "react-circular-progressbar";
+
+import MoreVert from "@mui/icons-material/MoreVert";
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVert fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} />
+        </div>
+        <p className="title">Total sale today</p>
+        <p className="amount">$420y</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be inclued
+        </p>
+        <div className="sumary">
+          <div className="item">
+            <div className="itemTitle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
